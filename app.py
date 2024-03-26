c8da8f7201b7ba19ce11f229843c528fe7cb8748
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-03-26 20:20:09 +0000

    desktop layout v1

--- app.py
+++ app.py
@@ -7,26 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route("/schedule")
 def schedule():
     return render_template("schedule.html")
-
 
 @app.route("/trainers")
 def trainers():
     return render_template("trainers.html")
 
-
 @app.route("/videos")
 def videos():
     return render_template("videos.html")
-
-
-# @app.route("/festival")
-# def festival():
-#     return render_template("festival.html")
-
 
 @app.route("/contact")
 def contact():
@@ -34,4 +25,5 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run()
+    # app.run(debug=True, port=8080)
