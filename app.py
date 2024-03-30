a012e6dcaf0256cdefe6a9756be0870730930734
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-03-30 14:55:03 +0000

    updating app.py v1.2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+import json
+import os
 
 app = Flask(__name__)
 
@@ -23,13 +25,24 @@ def videos():
 def contact():
     return render_template("contact.html")
 
+def load_users(file_path: str) -> dict:                                
+                                                            
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+    return data
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    json_file_path = os.path.join(app.static_folder, 'json', 'user.json')
+    user_list = load_users(json_file_path)
+    print(user_list)
     return render_template("login.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template("signup.html")
+
+
 
 
 if __name__ == "__main__":
